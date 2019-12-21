74f2704115e63b9c5f27bbf71b129cc1134bd4e0
Gaylor Bosson <gaylor.bosson@gmail.com>
2019-12-21 15:59:53 +0100

    update(treasure hunt): handle multi-guild setup

--- commands/treasure-hunt.js
+++ commands/treasure-hunt.js
@@ -26,15 +26,21 @@ function readDistantFile(url) {
   });
 }
 
-function findMember(message) {
+// Look for all the guilds that the author of the message and the bot share. It
+// then returns the member object for each guild matching.
+function findMutualGuilds(message) {
   const { client, author } = message;
 
-  const key = client.guilds.findKey(g => g.members.has(author.id));
-  if (!key) {
-    return undefined;
-  }
+  const members = client.guilds.reduce((acc, guild) => {
+    const member = guild.members.find(m => m.id === author.id);
+    if (member) {
+      return [...acc, member];
+    }
 
-  return client.guilds.get(key).members.get(author.id);
+    return acc;
+  }, []);
+
+  return members.filter(m => m.hasPermission(Permissions.FLAGS.ADMINISTRATOR));
 }
 
 exports.startTreasureHunt = async (message) => {
@@ -45,14 +51,10 @@ exports.startTreasureHunt = async (message) => {
     return Promise.resolve();
   }
 
-  const member = findMember(message);
-  if (!member) {
+  const members = findMutualGuilds(message);
+  if (members.length === 0) {
     logger.error('no guild in common with the user');
     return Promise.resolve();
-  }
-
-  if (!member.hasPermission(Permissions.FLAGS.ADMINISTRATOR)) {
-    logger.error('user is not an administrator');
   }
 
   const ma = attachments.first();
@@ -68,8 +70,10 @@ exports.startTreasureHunt = async (message) => {
   // TODO: stream
   const content = await readDistantFile(ma.url);
   await db.transaction(async (tx) => {
+    const ids = members.map(m => m.guild.id);
+
     await tx(TABLE_TREASURE_HUNT)
-      .where('guild_id', member.guild.id)
+      .whereIn('guild_id', ids)
       .delete();
 
     const promises = content.split(/\r?\n/).map((line) => {
@@ -78,39 +82,43 @@ exports.startTreasureHunt = async (message) => {
         return Promise.resolve();
       }
 
-      return tx(TABLE_TREASURE_HUNT).insert({
-        guild_id: member.guild.id,
+      return tx(TABLE_TREASURE_HUNT).insert(members.map(m => ({
+        guild_id: m.guild.id,
         key: key.trim(),
         value: value.trim(),
-      });
+      })));
     });
 
     await Promise.all(promises);
   });
 
-  await message.reply('A new treasure hunt has started !');
+  const guilds = members.map(m => m.guild.name).join(', ');
+  await message.reply(`A new treasure hunt has started for guilds: ${guilds}`);
 
   return Promise.resolve();
 };
 
 exports.askTreasureHunt = async (message) => {
-  if (!match(message)) {
+  if (!match(message) || message.content === '') {
     // ignore everything not a direct message to the bot.
     return Promise.resolve();
   }
 
-  const member = findMember(message);
-  if (!member) {
+  const members = findMutualGuilds(message);
+  if (members.length === 0) {
     return Promise.resolve();
   }
 
   const line = await db(TABLE_TREASURE_HUNT)
     .select()
-    .where({ guild_id: member.guild.id, key: message.content })
+    .whereIn('guild_id', members.map(m => m.guild.id))
+    .where({ key: message.content })
     .first();
 
   if (line) {
     message.reply(line.value);
+  } else {
+    message.reply('Essayez encore!');
   }
 
   return Promise.resolve();
