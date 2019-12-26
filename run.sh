e26f67e93e07dbaa0e3ef5c697e78cbc5ed1ca67
Gaylor Bosson <gaylor.bosson@gmail.com>
2019-12-26 18:12:59 +0100

    Fix run script

--- run.sh
+++ run.sh
@@ -4,5 +4,7 @@ if [ -f running_pid ]; then
   kill $(cat running_pid)
 fi
 
-nohup npm start >> /var/log/discord-alpha-bot/current.log 2>&1 &
+NODE_ENV=production
+node index.js >> /var/log/discord-alpha-bot/current.log 2>&1 &
+
 echo $! > running_pid
